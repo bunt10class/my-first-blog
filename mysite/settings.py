1afe74f2e9f7ed683ec3125d3c09e6fbed1bfdcc
bunt10class <kali-soda@rambler.ru>
2019-02-12 17:20:13 +0400

    Norm changes for life

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8jihh(m&0x@v9w75@22nn3n^f4ddf))ngd0n2#b!!5lv0b=@j4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','Bujhm.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1','Bujhm.pythonanywhere.com','bunt10class.pythonanywhere.com']
 
 
 # Application definition
